a93fac01d9d7ea1ddd65a5bead19d1cce9ba019c
i-muiri <ianmuiri99@gmail.com>
2022-12-14 15:16:55 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -9,9 +11,10 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    fetchBooks()
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  
 });
